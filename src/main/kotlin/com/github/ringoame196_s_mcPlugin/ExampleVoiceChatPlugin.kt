cc77ffeef8ff47548da113df2c75e875d6a27e93
りんご飴196 <kazumac41@gmail.com>
2025-01-18 13:52:35 +0900

    ✨  :SimpleVoiceChat関係

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/ExampleVoiceChatPlugin.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/ExampleVoiceChatPlugin.kt
@@ -6,31 +6,31 @@ import de.maxhenkel.voicechat.api.events.EventRegistration
 import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent
 
 class ExampleVoiceChatPlugin : VoicechatPlugin {
-	/**
-	 * @return the unique ID for this voice chat plugin
-	 */
-	override fun getPluginId(): String {
-		return "SimpleVCTransceiver"
-	}
+    /**
+     * @return the unique ID for this voice chat plugin
+     */
+    override fun getPluginId(): String {
+        return "SimpleVCTransceiver"
+    }
 
-	/**
-	 * Called when the voice chat initializes the plugin.
-	 *
-	 * @param api the voice chat API
-	 */
+    /**
+     * Called when the voice chat initializes the plugin.
+     *
+     * @param api the voice chat API
+     */
 
-	override fun initialize(api: VoicechatApi?) {
-	}
+    override fun initialize(api: VoicechatApi?) {
+    }
 
-	/**
-	 * Called once by the voice chat to register all events.
-	 *
-	 * @param registration the event registration
-	 */
+    /**
+     * Called once by the voice chat to register all events.
+     *
+     * @param registration the event registration
+     */
 
-	override fun registerEvents(registration: EventRegistration?) {
-		// registration?.registerEvent(MicrophonePacketEvent::class.java, this::onMicrophonePacket)
-	}
+    override fun registerEvents(registration: EventRegistration?) {
+        // registration?.registerEvent(MicrophonePacketEvent::class.java, this::onMicrophonePacket)
+    }
 
-	private fun onMicrophonePacket(e: MicrophonePacketEvent) {}
+    private fun onMicrophonePacket(e: MicrophonePacketEvent) {}
 }
